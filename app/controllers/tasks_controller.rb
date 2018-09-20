8f5dded6f60eff742734ab677204a6c1aae1622c
Amanda Ungco <amanda.ungco@gmail.com>
2018-09-19 18:18:16 -0700

    added edit, delete, mark as complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,26 +14,54 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new(name: "Homework")
+    @task = Task.new
   end
 
   def edit
     id = params[:id]
     @task = Task.find_by(id:id)
-    puts
+    render :new
   end
 
   def update
     id = params[:id]
     @task = Task.find_by(id:id)
+    @task.name = params[:task][:name]
+    @task.description = params[:task][:description]
+    @task.completion_date = params[:task][:completion_date]
+    if @task.save
+      redirect_to task_path
+    else
+      render :new
+    end
+
   end
 
   def destroy
     id = params[:id]
     @task = Task.find_by(id:id)
+    @task.destroy
+      redirect_to root_path
   end
 
   def create
+    @task = Task.new(name: params[:task][:name])
+      if @task.save
+        redirect_to root_path
+      else
+        render :new
+      end
   end
 
+  def status
+    id = params[:id]
+    @task = Task.find_by(id:id)
+    if @task.status.class == String
+      @task.status = nil
+    else @task.status == nil
+      @task.status = "Completed on #{Date.today}"
+    end
+      @task.save
+      redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-  patch '/tasks/:id', to: 'books#update'
-  delete '/tasks/:id', to: 'books#destroy', as: "delete_task"
+  patch '/tasks/:id/status', to: 'tasks#status', as: 'task_status'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy', as: "delete_task"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
