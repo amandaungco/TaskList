e2e39e801f070ea38b9c27fabfac809a6c12aac8
Amanda Ungco <amanda.ungco@gmail.com>
2018-09-21 15:06:05 -0700

    played with css

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,4 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   patch '/tasks/:id/status', to: 'tasks#status', as: 'task_status'
-#   get '/tasks', to: 'tasks#index', as: 'tasks'
-#   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-#   post '/tasks', to: 'tasks#create'
-#
-#   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-#   get '/tasks/:id', to: 'tasks#show', as: 'task'
-#   patch '/tasks/:id/status', to: 'tasks#status', as: 'task_status'
-  # patch '/tasks/:id', to: 'tasks#update'
-#   delete '/tasks/:id', to: 'tasks#destroy', as: "delete_task"
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
